40d8999ab18a990ffd80f4e192c6db8690924dbc
CastleBomber <richie.ucsb@gmail.com>
2020-11-15 10:41:04 -0800

    page171

--- Unity2D ItemManagement/Assets/Scripts/MonoBehaviour/Player.cs
+++ Unity2D ItemManagement/Assets/Scripts/MonoBehaviour/Player.cs
@@ -4,5 +4,37 @@ using UnityEngine;
 
 public class Player : Character
 {
-    
+	void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.gameObject.CompareTag("CanBePickedUp"))
+		{
+			Item hitObject = collision.gameObject.GetComponent<Consumable>().item;
+
+			if(hitObject != null)
+			{
+				print("hit: " + hitObject.objectName);
+
+				switch (hitObject.itemType)
+				{
+					case Item.ItemType.COIN:
+						break;
+
+					case Item.ItemType.HEALTH:
+						AdjustHitPoints(hitObject.quantity);
+						break;
+					default:
+						break;
+				}
+
+				collision.gameObject.SetActive(false);
+			}
+			
+		}
+	}
+
+	public void AdjustHitPoints (int amount)
+	{
+		hitPoints += amount;
+		print("Adjusted hitpoints by: " + amount + ". New value: " + hitPoints);
+	}
 }
