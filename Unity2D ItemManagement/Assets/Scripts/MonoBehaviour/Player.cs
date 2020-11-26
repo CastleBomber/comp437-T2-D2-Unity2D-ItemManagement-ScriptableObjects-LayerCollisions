9102b84fb71fef65cc434a031ace2939e09eeb91
CastleBomber <richie.ucsb@gmail.com>
2020-11-26 11:43:30 -0800

    coinsStackable

--- Unity2D ItemManagement/Assets/Scripts/MonoBehaviour/Player.cs
+++ Unity2D ItemManagement/Assets/Scripts/MonoBehaviour/Player.cs
@@ -1,63 +1,60 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Player : Character
 {
-	public HealthBar healthBarPrefab;
-	HealthBar healthBar;
-	public Inventory inventoryPrefab;
-	Inventory inventory;
+    public Inventory inventoryPrefab;
+    Inventory inventory;
 
-	public void Start()
-	{
-		inventory = Instantiate(inventoryPrefab);
-		hitPoints.value = startingHitPoints;
-		healthBar = Instantiate(healthBarPrefab);
-		healthBar.character = this;
-	}
+    public HealthBar healthBarPrefab;
+    HealthBar healthBar;
 
-	void OnTriggerEnter2D(Collider2D collision)
-	{
-		if (collision.gameObject.CompareTag("CanBePickedUp"))
-		{
-			Item hitObject = collision.gameObject.GetComponent<Consumable>().item;
+    public void Start()
+    {
+        hitPoints.value = startingHitPoints;
+        inventory = Instantiate(inventoryPrefab);
+        healthBar = Instantiate(healthBarPrefab);
+        healthBar.character = this;
+    }
 
-			if(hitObject != null)
-			{
-				bool shouldDisappear = false;
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("CanBePickedUp"))
+        {
+            Item hitObject = collision.gameObject.GetComponent<Consumable>().item;
 
-				switch (hitObject.itemType)
-				{
-					case Item.ItemType.COIN:
-						shouldDisappear = inventory.AddItem(hitObject);
-						//shouldDisappear = true;
-						break;
+            if (hitObject != null)
+            {
+                bool shouldDisappear = false;
 
-					case Item.ItemType.HEALTH:
-						shouldDisappear = AdjustHitPoints(hitObject.quantity);
-						break;
-					default:
-						break;
-				}
+                switch (hitObject.itemType)
+                {
+                    case Item.ItemType.COIN:
+                        shouldDisappear = inventory.AddItem(hitObject);
+                        break;
+                    case Item.ItemType.HEALTH:
+                        shouldDisappear = AdjustHitPoints(hitObject.quantity);
+                        break;
+                    default:
+                        break;
+                }
 
-				if (shouldDisappear)
-				{
-					collision.gameObject.SetActive(false);
-				}
-			}
-			
-		}
-	}
+                if (shouldDisappear)
+                {
+                    collision.gameObject.SetActive(false);
+                }
+            }
+        }
+    }
 
-	public bool AdjustHitPoints (int amount)
-	{
-		if (hitPoints.value < maxHitPoints)
-		{
-			hitPoints.value += amount;
-			print("Adjusted hitpoints by: " + amount + ". New value: " + hitPoints.value);
-		}
-
-		return true;
-	}
+    public bool AdjustHitPoints(int amount)
+    {
+        if (hitPoints.value < maxHitPoints)
+        {
+            hitPoints.value = hitPoints.value + amount;
+            print("Adjusted HP by: " + amount + ". New value: " + hitPoints.value);
+            return true;
+        }
+        print("didnt adjust hitpoints");
+        return false;
+    }
 }
